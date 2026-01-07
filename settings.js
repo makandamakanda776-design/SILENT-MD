a691a0f3b436d304e80528ecabd4b1ad6bc5722e
makandamakanda776-design <makandamakanda776@gmail.com>
2026-01-07 11:47:56 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'SILENT Bot',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botName: "Silent Bot",
+  botOwner: 'Nodie', // Your name
+  ownerNumber: '263784549656', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
